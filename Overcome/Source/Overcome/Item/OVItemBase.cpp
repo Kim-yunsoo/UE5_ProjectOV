0de7496f46cf536d5c7dff2e343c5ef3583e4c23
YUN SOO KIM <118554050+Kim-yunsoo@users.noreply.github.com>
2024-05-17 21:52:19 +0900

    ItemBase Code 작성

--- Overcome/Source/Overcome/Item/OVItemBase.cpp
+++ Overcome/Source/Overcome/Item/OVItemBase.cpp
@@ -3,3 +3,34 @@
 
 #include "Item/OVItemBase.h"
 
+UOVItemBase::UOVItemBase()
+{
+}
+
+UOVItemBase* UOVItemBase::CreateItemCopy() const  //원본을 복사한다. 그 사본을 리턴! 
+{
+	UOVItemBase* ItemCopy = NewObject<UOVItemBase>(StaticClass()); //정적 클래스를 만든다.
+	ItemCopy->ID=this->ID;
+	ItemCopy->Quantity = this->Quantity;
+	ItemCopy->ItemType = this->ItemType;
+	ItemCopy->TextData = this->TextData;
+	ItemCopy->ItemNumericData = this->ItemNumericData;
+	ItemCopy->ItemStatistics = this->ItemStatistics;
+	ItemCopy->AssetData = this -> AssetData;
+
+	return ItemCopy;
+	
+}
+
+void UOVItemBase::SetQuantity(const int32 NewQuantity)
+{
+	if(NewQuantity!=Quantity)
+	{
+		Quantity = FMath::Clamp(NewQuantity,0,ItemNumericData.bIsStackable? ItemNumericData.MaxStackSize:1);
+		//bIsStackable가 true인경우 아닌경우 
+	}
+}
+
+void UOVItemBase::Use(AOVCharacterPlayer* Character)
+{
+}
--- Overcome/Source/Overcome/Item/OVItemBase.h
+++ Overcome/Source/Overcome/Item/OVItemBase.h
@@ -3,15 +3,63 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/NoExportTypes.h"
+//#include "Character/OVCharacterPlayer.h"
+#include "Item/OVItemData.h"
 #include "OVItemBase.generated.h"
 
 /**
  * 
  */
+class AOVCharacterPlayer;
 UCLASS()
 class OVERCOME_API UOVItemBase : public UObject
 {
 	GENERATED_BODY()
+
+public:
+
+	// UPROPERTY()
+	// UInventoryComponent* OwningInventory;
+
+	UPROPERTY(VisibleAnywhere,Category = "Item Data", meta= (UIMin=1, UIMax=100))
+	int32 Quantity; //아이템 수량
+	
+	UPROPERTY(EditAnyWhere,Category="Item Data")
+	FName ID;
+
+	UPROPERTY(EditAnyWhere,Category="Item Data")
+	EItemType ItemType;
+
+	UPROPERTY(EditAnyWhere,Category="Item Data")
+	FItemStatistics ItemStatistics;
+
+	UPROPERTY(EditAnyWhere,Category="Item Data")
+	FItemTextData TextData;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FItemNumericData ItemNumericData;
+
+	UPROPERTY(EditAnyWhere,Category="Item Data")
+	FItemAssetData AssetData;
+
+	UOVItemBase();
+
+	UOVItemBase* CreateItemCopy() const;
+
+	//FORCEINLINE float GetItemStackWeight() const{return Quantity* ItemNumericData.Weight};  무게 코드
+	UFUNCTION(Category = "Item")
+	FORCEINLINE bool IsFullItemStack() const { return Quantity == ItemNumericData.MaxStackSize;}; // 수량이 최대 스택크기와 같은지?
+
+	UFUNCTION(Category = "Item")
+	void SetQuantity(const int32 NewQuantity);
+	
+	UFUNCTION(Category = "Item") //캐리터를 가져온다. 
+	virtual void Use(AOVCharacterPlayer* Character);
+
+protected:
+	bool operator==(const FName& OtherID) const
+	{
+		return ID == OtherID;
+	}
 	
 };
