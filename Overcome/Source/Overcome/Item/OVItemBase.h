84dac9e246a530833ec31b8c5ea86810bd202327
YUN SOO KIM <118554050+Kim-yunsoo@users.noreply.github.com>
2024-05-17 19:13:20 +0900

    ItemBase 만들기

--- Overcome/Source/Overcome/Item/OVItemBase.h
+++ Overcome/Source/Overcome/Item/OVItemBase.h
@@ -3,31 +3,15 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
+#include "UObject/NoExportTypes.h"
 #include "OVItemBase.generated.h"
 
+/**
+ * 
+ */
 UCLASS()
-class OVERCOME_API AOVItemBase : public AActor
+class OVERCOME_API UOVItemBase : public UObject
 {
 	GENERATED_BODY()
 	
-public:	
-	// Sets default values for this actor's properties
-	AOVItemBase();
-
-protected:
-	UPROPERTY(VisibleAnywhere, Category = Item)
-	TObjectPtr<class UBoxComponent> Trigger;
-
-	UPROPERTY(VisibleAnywhere, Category = Item)
-	TObjectPtr<class UStaticMeshComponent> Mesh;
-
-	UPROPERTY(VisibleAnywhere, Category = Effect)
-	TObjectPtr<class UParticleSystemComponent> Effect;
-
-	// UPROPERTY(EditAnywhere, Category = Item)
-	// TObjectPtr<class UOVItemData> Item;
-	// 	
-	UFUNCTION()
-	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
