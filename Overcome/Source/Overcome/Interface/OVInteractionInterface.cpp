15b92616fabcb6aab36dd4285dd0f25d2498bbbc
YUN SOO KIM <118554050+Kim-yunsoo@users.noreply.github.com>
2024-05-17 22:37:19 +0900

    interaction interface 구성

---
+++ Overcome/Source/Overcome/Interface/OVInteractionInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interface/OVInteractionInterface.h"
+
+// Add default functionality here for any IOVInteractionInterface functions that are not pure virtual.
