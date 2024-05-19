ac7a4d5825397683bf4772126136da4e1d4e265e
YUN SOO KIM <118554050+Kim-yunsoo@users.noreply.github.com>
2024-05-19 15:28:28 +0900

    내적을 이용해 라인트레이스가 캐릭터의 시선방향으로만 나가도록 로직

--- Overcome/Source/Overcome/Character/OVCharacterPlayer.cpp
+++ Overcome/Source/Overcome/Character/OVCharacterPlayer.cpp
@@ -130,6 +130,7 @@ AOVCharacterPlayer::AOVCharacterPlayer()
 
 	InteractionCheckFrequency=0.1f;
 	InteractionCheckDistance =225.0f;
+	BaseEyeHeight = 74.0f; 
 	TurningInPlace = ETurningPlaceType::ETIP_NotTurning;
 }
 
@@ -623,37 +624,44 @@ void AOVCharacterPlayer::Tick(float DeltaSeconds)
 }
 
 void AOVCharacterPlayer::PerformInteractionCheck()
-{ //todo
+{
+	//todo
 	InteractionData.LastInteractionCheckTime = GetWorld()->GetTimeSeconds();
 
 	FVector TraceStart{GetPawnViewLocation()}; //폰의 뷰 위치가 시작 자동으로 눈 높의를 나타내는 특정 값으로 초기화
 	FVector TraceEnd {TraceStart + (GetViewRotation().Vector()*InteractionCheckDistance)}; //캐릭터 컨트롤러의 회전 값
 
-	DrawDebugLine(GetWorld(), TraceStart,TraceEnd,FColor::Red,false,1.0f , 0,2.0f);
+	float LookDirection = FVector::DotProduct(GetActorForwardVector(), GetViewRotation().Vector());
 
-	FCollisionQueryParams QueryParams;
-	QueryParams.AddIgnoredActor(this);
-	FHitResult TraceHit;
-
-	if(GetWorld()->LineTraceSingleByChannel(TraceHit,TraceStart,TraceEnd,ECC_Visibility,QueryParams))
+	if(LookDirection>0) //시선 벡터가 양수인 경우 앞을 보고 있다는 의미니까! 
 	{
-		if(TraceHit.GetActor()->GetClass()->ImplementsInterface(UOVInteractionInterface::StaticClass()))
+		DrawDebugLine(GetWorld(), TraceStart, TraceEnd, FColor::Red, false, 1.0f, 0, 2.0f);
+
+		FCollisionQueryParams QueryParams;
+		QueryParams.AddIgnoredActor(this);
+		FHitResult TraceHit;
+
+		if (GetWorld()->LineTraceSingleByChannel(TraceHit, TraceStart, TraceEnd, ECC_Visibility, QueryParams))
 		{
-			const float Distance = (TraceStart - TraceHit.ImpactPoint).Size();
-
-			if(TraceHit.GetActor() != InteractionData.CurrentInteractable&& Distance <= InteractionCheckDistance)
+			if (TraceHit.GetActor()->GetClass()->ImplementsInterface(UOVInteractionInterface::StaticClass()))
 			{
-				FoundInteractable(TraceHit.GetActor()); // 상호작용 가능한 액터를 찾음
-				return;
-			}
+				//특정 인터페이스가 있는지 확인 
+				const float Distance = (TraceStart - TraceHit.ImpactPoint).Size();
 
-			if(TraceHit.GetActor() == InteractionData.CurrentInteractable)
-			{
-				return; // 여전히 같은 것을 보고 있다
+				if (TraceHit.GetActor() != InteractionData.CurrentInteractable && Distance <= InteractionCheckDistance)
+				{
+					FoundInteractable(TraceHit.GetActor()); // 상호작용 가능한 액터를 찾음
+					return;
+				}
+
+				if (TraceHit.GetActor() == InteractionData.CurrentInteractable)
+				{
+					return; // 여전히 같은 것을 보고 있다
+				}
 			}
 		}
+		NoInteractableFound(); //아무것도 못찾은 경우 반환 
 	}
-	NoInteractableFound(); //아무것도 못찾은 경우 반환 
 }
 
 void AOVCharacterPlayer::FoundInteractable(AActor* NewInteractable)
