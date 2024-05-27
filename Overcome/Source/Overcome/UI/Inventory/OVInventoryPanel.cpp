a08a27592224922b5c0c24a7df0bb0cbee35ed9c
YUN SOO KIM <118554050+Kim-yunsoo@users.noreply.github.com>
2024-05-27 20:33:47 +0900

    인벤토리 패널 작업중

--- Overcome/Source/Overcome/UI/Inventory/OVInventoryPanel.cpp
+++ Overcome/Source/Overcome/UI/Inventory/OVInventoryPanel.cpp
@@ -3,3 +3,52 @@
 
 #include "UI/Inventory/OVInventoryPanel.h"
 
+#include "OVInventoryItemSlot.h"
+#include "Character/OVCharacterPlayer.h"
+#include "Component/OVInventoryComponent.h"
+#include "Components/TextBlock.h"
+#include "Components/WrapBox.h"
+
+void UOVInventoryPanel::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	PlayerCharacter = Cast<AOVCharacterPlayer>(GetOwningPlayerPawn());
+
+	if(PlayerCharacter)
+	{
+		InventoryReference = PlayerCharacter->GetInventory();
+		if(InventoryReference)
+		{
+			InventoryReference->OnInventoryUpdated.AddUObject(this, &UOVInventoryPanel::RefreshInventory);
+			//인벤토리 컴포넌트 델리게이트와 연결
+			SetInfoText();
+			
+		}
+	}
+	
+}
+void UOVInventoryPanel::SetInfoText() const
+{
+	CapacityInfo->SetText(FText::Format(FText::FromString("{0}/{1}"),
+		InventoryReference->GetInventoryContents().Num(),
+		InventoryReference->GetSlotsCapacity()));
+}
+
+void UOVInventoryPanel::RefreshInventory()
+{
+	if(InventoryReference && InventorySlotClass)
+	{
+		InventoryPanel->ClearChildren(); // 추가하기 전에 자식 UI를 정리
+
+		for 
+		
+		
+	}
+}
+
+
+bool UOVInventoryPanel::NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent,
+	UDragDropOperation* InOperation)
+{
+	return Super::NativeOnDrop(InGeometry, InDragDropEvent, InOperation);
+}
--- Overcome/Source/Overcome/UI/Inventory/OVInventoryPanel.h
+++ Overcome/Source/Overcome/UI/Inventory/OVInventoryPanel.h
@@ -19,11 +19,22 @@ class OVERCOME_API UOVInventoryPanel : public UUserWidget
 {
 	GENERATED_BODY()
 
+	UFUNCTION()
+	void RefreshInventory();
+	UPROPERTY(meta=(BindWidget))
 	TObjectPtr<UWrapBox> InventoryPanel;
+	UPROPERTY(meta=(BindWidget))
 	TObjectPtr<UTextBlock> CapacityInfo;
+	UPROPERTY()
 	TObjectPtr<AOVCharacterPlayer> PlayerCharacter;
+	UPROPERTY()
 	TObjectPtr<UOVInventoryComponent> InventoryReference;
-
+	UPROPERTY(EditDefaultsOnly)
 	TSubclassOf<UOVInventoryItemSlot> InventorySlotClass;
-	
+
+protected:
+	void SetInfoText() const ;
+	virtual void NativeOnInitialized() override;
+	virtual bool NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent,
+		UDragDropOperation* InOperation) override;
 };
